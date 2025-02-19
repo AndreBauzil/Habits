f5e7a0728581efa196964cc144ba73abd1c1d796
AndreBauzil <Andregbauzil@gmail.com>
2025-02-18 21:42:22 -0300

    fix: font family to used font and path to page content

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.tsx",
+    "./index.html",
   ],
   theme: {
     extend: {
@@ -10,10 +11,7 @@ module.exports = {
         background: '#09090a',
       },
       fontFamily: {
-        regular: 'Inter_400Regular',
-        semibold: 'Inter_600SemiBold',
-        bold: 'Inter_700Bold',
-        extrabold: 'Inter_800ExtraBold'
+        Inter: ['Inter', 'sans-serif'],
       }
     },
   },
