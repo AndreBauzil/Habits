3d0873459c0e8003541e78385d78439d402ce7db
AndreBauzil <Andregbauzil@gmail.com>
2025-03-11 18:29:44 -0300

    style: add minor changes to the code, to allow using the checkbox component as a button, with onPress

--- mobile/src/components/Checkbox.tsx
+++ mobile/src/components/Checkbox.tsx
@@ -1,17 +1,18 @@
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, TouchableOpacityProps, View } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import colors from "tailwindcss/colors";
 
-interface Props {
+interface Props extends TouchableOpacityProps {
     title: string;
     checked?: boolean;
 }
 
-export function Checkbox({ title, checked = false }: Props) {
+export function Checkbox({ title, checked = false, ...rest }: Props) {
   return (
     <TouchableOpacity
         activeOpacity={0.7}
         className="flex-row mb-2 items-center"
+        {...rest}
     >
     {   
     checked ? 
@@ -26,7 +27,7 @@ export function Checkbox({ title, checked = false }: Props) {
         <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
     }
 
-    <Text className="text-white text-base ml-3">
+    <Text className="text-white text-base ml-3 font-semibold">
         { title }
     </Text>
     </TouchableOpacity>
