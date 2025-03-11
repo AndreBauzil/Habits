74ec36fec8c80f64076e620a5f28becb0440bd26
AndreBauzil <Andregbauzil@gmail.com>
2025-03-11 18:36:50 -0300

    style: add some minor styling changes to show placeholder on the input, the confirmation button at the bottom and the checkboxes for each weekDay

--- mobile/src/Screens/New.tsx
+++ mobile/src/Screens/New.tsx
@@ -1,7 +1,10 @@
-import { ScrollView, View, Text, TextInput } from "react-native";
-import { BackButton } from "../components/BackButton";
-import { Checkbox } from "../components/Checkbox";
 import { useState } from "react";
+import { ScrollView, View, Text, TextInput, TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import colors from "tailwindcss/colors";
+
+import { Checkbox } from "../components/Checkbox";
+import { BackButton } from "../components/BackButton";
 
 const avaliableWeekDays = ['Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sábado'];
 
@@ -18,7 +21,10 @@ export function New() {
 
   return (
     <View className="flex-1 bg-background px-8 pt-16">
-        <ScrollView showsVerticalScrollIndicator={false}>
+        <ScrollView 
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 100 }}
+        >
 
           <BackButton />
 
@@ -31,7 +37,9 @@ export function New() {
           </Text>
 
           <TextInput 
-            className="h-12 pl-4 rounded-lg mt-3 bg-zinc-5800 text-white focus:border-2 focus:border-green-600" 
+            className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600" 
+            placeholder="Exercícios, Dormir 8hrs, etc"
+            placeholderTextColor={colors.zinc[400]}
           />
 
           <Text className="font-semibold mt-4 mb-3 text-white text-base">
@@ -43,10 +51,25 @@ export function New() {
               <Checkbox 
                 key={weekDay}
                 title={weekDay}
+                checked={weekDays.includes(index)}
+                onPress={() => handleToggleWeekDay(index)}
               />
             ))
           }
+          <TouchableOpacity
+            className="w-full h-14 flex-row items-center bg-green-600 rounded-md mt-6"
+            activeOpacity={0.7}
+          >
+            <Feather 
+              name="check"
+              size={20}
+              color={colors.white}
+            />
 
+            <Text className="font-semibold text-base text-white ml-2">
+              Confirmar
+            </Text>
+          </TouchableOpacity>
         </ScrollView>
     </View>
   )
