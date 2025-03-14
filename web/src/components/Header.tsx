1df7af77c61da81ebf575e0abc1cbe930da2cce2
AndreBauzil <Andregbauzil@gmail.com>
2025-03-13 23:40:45 -0300

    style: some minor code and writing fixes

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -5,7 +5,6 @@ import logoImage from '../assets/logo.svg';
 import { NewHabitForm } from "./NewHabitForm";
 
 
-
 export function Header() {
     return (
         <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
