27cc9ae9bd85c89bf16c5e516a12f43bb5b3105d
AndreBauzil <Andregbauzil@gmail.com>
2025-03-24 16:38:44 -0300

    fix: add a offset from the start of the year to Sunday, to align the days with the summary table

--- mobile/src/utils/generate-range-between-dates.ts
+++ mobile/src/utils/generate-range-between-dates.ts
@@ -4,8 +4,11 @@ export function generateRangeDatesFromYearStart() {
   const startDate = dayjs().startOf('year')
   const endDate = new Date()
 
+  const offset = (startDate.day()) % 7; 
+
   let dateRange = []
-  let compareDate = startDate
+  let compareDate = startDate.subtract(offset, 'days')
+1
 
   while (compareDate.isBefore(endDate)) {
     dateRange.push(compareDate.toDate())
--- web/src/utils/generate-range-between-dates.ts
+++ web/src/utils/generate-range-between-dates.ts
@@ -4,8 +4,12 @@ export function generateRangeDatesFromYearStart() {
   const startDate = dayjs().startOf('year')
   const endDate = new Date()
 
+  // Calcula o offset para chegar na quarta-feira (dia 1o)
+  const offset = (startDate.day()) % 7; 
+
   let dateRange = []
-  let compareDate = startDate
+  let compareDate = startDate.subtract(offset, 'days')
+
 
   while (compareDate.isBefore(endDate)) {
     dateRange.push(compareDate.toDate())
